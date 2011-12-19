3d68c8fc5e1e5be8fb5ac41c6e7b5ac87397dc70
Taddeus Kroes <taddeuskroes@hotmail.com>
2011-12-19 23:29:43 +0100

    Added blank unit test file for optimize.py.

---
+++ tests/test_optimize.py
@@ -0,0 +1,13 @@
+import unittest
+
+from src.optimize import optimize_global
+from src.utils import Statement as S, Block as B
+
+
+class TestOptimize(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_(self):
+        pass
