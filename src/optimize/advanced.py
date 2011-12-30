f971a60ed2e9b428af8142bc388282387bdfc7c6
Jayke Meijer <jayke.meijer@gmail.com>
2011-12-30 11:40:43 +0100

    Added check if li already has int as argument.

--- src/optimize/advanced.py
+++ src/optimize/advanced.py
@@ -141,7 +141,10 @@ def fold_constants(block):
 
         if s.name == 'li':
             # Save value in register
-            register[s[0]] = int(s[1], 16)
+            if not isinstance(s[1], int):
+                register[s[0]] = int(s[1], 16)
+            else:
+                register[s[0]] = s[1]
             known.append((s[0], register[s[0]]))
         elif s.name == 'move' and s[0] in register:
             reg_to, reg_from = s
